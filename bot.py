f0133632d4ea8c4ddc332c75f36c035dc5af93d6
vireon75-ctrl <vireon75@gmail.com>
2026-08-12 15:38:46 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-43from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Application,
     CommandHandler,
